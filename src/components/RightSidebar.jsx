e22cb50c8f30f2d3f065445d94879df1ffad1d9e
sauren-sharma <sauren.sharma@zenstreet.ai>
2025-02-18 20:23:21 +0530

    UI:Right side bar

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,6 +1,31 @@
+import assets from "../assets/assets";
 import "./rightsidebar.css";
 const RightSidebar = () => {
-  return <div></div>;
+  return (
+    <div className="rs">
+      <div className="rs-profile">
+        <img src={assets.profile_img} alt="" />
+        <h3>
+          Name <img src={assets.green_dot} className="dot" alt="" />
+        </h3>
+        <p>Hey, I am Name using chat app</p>
+      </div>
+      <hr />
+      <div className="rs-media">
+        <p>Media</p>
+        <div>
+          <img src={assets.pic1} alt="" />
+          <img src={assets.pic2} alt="" />
+          <img src={assets.pic3} alt="" />
+          <img src={assets.pic4} alt="" />
+          <img src={assets.pic5} alt="" />
+          <img src={assets.pic6} alt="" />
+        </div>
+      </div>
+
+      <button>Logout</button>
+    </div>
+  );
 };
 
 export default RightSidebar;
